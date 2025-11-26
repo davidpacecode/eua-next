451f1319e625efcb3adf94108c37c65826b94f9c
davidpacecode <davidpace@gmail.com>
2025-11-26 12:39:39 -0600

    have basic lt functionality working

--- config/initializers.rb
+++ config/initializers.rb
@@ -103,7 +103,7 @@ Bridgetown.configure do |config|
   # For more documentation on how to configure your site using this initializers file,
   # visit: https://edge.bridgetownrb.com/docs/configuration/initializers/
 
-  collections_dir "my_collections"
+  collections_dir: "my_collections"
 
   collections do
     lt_members do
